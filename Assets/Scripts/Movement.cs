37f7ca1c5ce2116a8a03a8e65825c9be1b9048ae
seungdo1234 <wltmdeh12@naver.com>
2023-11-15 00:21:22 +0900

    23.11.15 (회전, 추력)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody rigid;
 
+    [SerializeField]
+    private float thrustPower;
+    [SerializeField]
+    private float rotionPower;
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -21,7 +25,9 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("스페이스 바 누름");
+            float thrust = thrustPower * Time.deltaTime;
+            // AddRelativeForce : 오브젝트의 로컬 좌표를 기준으로 힘을 부여 (회전이 된 상태일때 오브젝트의 머리부분으로 힘이 들어감)
+            rigid.AddRelativeForce(Vector3.up * thrust);
         }   
     }
 
@@ -29,11 +35,19 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("왼쪽 누름");
+
+            RotationThrust(rotionPower);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("오른쪽 누름");
+            RotationThrust(-rotionPower);
         }
+    }
+
+    public void RotationThrust(float rotationDir)
+    {
+        rigid.freezeRotation = true; // 수동 제어를 할 수 있도록 회전을 고정
+        transform.Rotate(Vector3.forward  * Time.deltaTime * rotationDir);
+        rigid.freezeRotation = false; // 물리 시스템이 적용 되도록 회전 고정을 해제
     }
 }
